d0017f93a576d1963abda6516b9adafede602ef4
xxxtomandersonxx <xxtomandersonzz@gmail.com>
2023-07-08 15:24:35 +0900

    Finish!!

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,8 @@
+import { getSongs } from "@/actions/getSongs";
 import { Header } from "@/components/Header";
 import { ListItem } from "@/components/ListItem";
+
 import { PageContent } from "./components/PageContent";
-import { getSongs } from "@/actions/getSongs";
 
 export const revalidate = 0;
 
@@ -9,32 +10,40 @@ export default async function Home() {
   const songs = await getSongs();
 
   return (
-    <div className="
-      bg-neutral-900
-      rounded-lg
-      h-full
-      w-full
-      overflow-hidden
-      overflow-y-auto
+    <div
+      className="
+        bg-neutral-900 
+        rounded-lg 
+        h-full 
+        w-full 
+        overflow-hidden 
+        overflow-y-auto
       "
     >
       <Header>
         <div className="mb-2">
-          <h1 className="text-white text-3xl font-semibold">
+          <h1
+            className="
+            text-white 
+              text-3xl 
+              font-semibold
+            ">
             Welcome back
           </h1>
-          <div className="
-            grid
-            grid-cols-1
-            sm:grid-cols-2
-            xl:grid-cols-3
-            2xl:grid-cols-4
-            gap-3
-            mt-4"
+          <div
+            className="
+              grid 
+              grid-cols-1 
+              sm:grid-cols-2 
+              xl:grid-cols-3 
+              2xl:grid-cols-4 
+              gap-3 
+              mt-4
+            "
           >
             <ListItem
-              image="/images/liked.png"
               name="Liked Songs"
+              image="/images/liked.png"
               href="liked"
             />
           </div>
@@ -46,9 +55,7 @@ export default async function Home() {
             Newest songs
           </h1>
         </div>
-        <div>
-          <PageContent songs={songs} />
-        </div>
+        <PageContent songs={songs} />
       </div>
     </div>
   )
